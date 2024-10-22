21d6aac27dbcf947032ff3306b075754a7a8efff
Jared Muñoz <amunozbaez669@gmail.com>
2024-10-22 13:33:58 -0600

    fix: set cookies as await to support nextjs 15

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -3,6 +3,6 @@
 import { cookies } from "next/headers";
 
 export const getCookie = async (key: string) => {
-	const cookie = cookies().get(key);
+	const cookie = (await cookies()).get(key);
 	return cookie?.value;
 };
